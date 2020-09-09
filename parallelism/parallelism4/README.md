1925d6305074e42a53efae692901d32066b6ad94
Eduardo Carneiro <eduardo.oc@gmail.com>
2020-09-09 00:57:22 -0300

    added parallelism4 script v1

---
+++ parallelism/parallelism4/README.md
@@ -0,0 +1,5 @@
+```text
+
+This parallelism4.sh will implement the number of thread, process the number of threads and wait each thread to finish to start another one, always keeping the program running in parallel the thread number.
+
+```
---
+++ parallelism/parallelism4/parallelism4.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+function get_infos()
+{
+	A=$1
+
+	for b in a b c 
+	do
+	#	echo "$A - resource $b" 	
+		touch $A-$b.txt 
+		sleep 2
+	done
+}
+
+
+
+# main program
+for project in $(seq 10)
+do
+	echo project-$project
+	get_infos $project &
+	echo $!
+	ps -p $!
+
+done
+
